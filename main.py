a98a12a50ebd6940100fdfc61c83496f3f7d819c
mr_X <mr.Aleksandrsmith@gmail.com>
2023-09-30 15:14:15 +0300

    center rect

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pygame
 import sys
-from random import randint
 
-clock = pygame.time.Clock()
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption('Excellent Shooter')
+
+fill_color = (32, 52, 71)
+
+rect_width, rect_height = 100, 200
+rect_x = screen_width / 2 - rect_width / 2
+rect_y = screen_height / 2 - rect_height / 2
+rect_color = pygame.Color('lightyellow')
 
 while True:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+    screen.fill(fill_color)
+    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
     pygame.display.update()
 
-    clock.tick(1)
+
 
 
 
