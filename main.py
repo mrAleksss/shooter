c91d9b61db3a243aee0bae6a21ff731e0c3542dd
mr_X <mr.Aleksandrsmith@gmail.com>
2023-10-03 15:32:28 +0300

    fighter move

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ pygame.display.set_caption('Aliens VS Fighter')
 fighter_image = pygame.image.load('images/fighter.png')
 fighter_width, fighter_height = fighter_image.get_size()
 fighter_x, fighter_y = ((screen_width / 2) - (fighter_width / 2)), screen_height - fighter_height
+FIGHTER_STEP = 10
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and fighter_x >= FIGHTER_STEP:
+                fighter_x -= FIGHTER_STEP
+            if event.key == pygame.K_RIGHT and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
+                fighter_x += FIGHTER_STEP
+
+                pass
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
 
