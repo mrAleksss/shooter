6d299bb7e6e675abf504bcc7112385c8dc877031
mr_X <mr.Aleksandrsmith@gmail.com>
2023-10-03 15:21:31 +0300

    spaceship

--- main.py
+++ main.py
@@ -5,28 +5,20 @@ import sys
 pygame.init()
 
 screen_width, screen_height = 800, 600
+screen_fill_color = (32, 52, 71)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption('Excellent Shooter')
+pygame.display.set_caption('Aliens VS Fighter')
 
-fill_color = (32, 52, 71)
-
-rect_width, rect_height = 100, 200
-rect_x = screen_width / 2 - rect_width / 2
-rect_y = screen_height / 2 - rect_height / 2
-rect_color = pygame.Color('lightyellow')
+fighter_image = pygame.image.load('images/fighter.png')
+fighter_width, fighter_height = fighter_image.get_size()
+fighter_x, fighter_y = ((screen_width / 2) - (fighter_width / 2)), screen_height - fighter_height
 
 while True:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.fill(fill_color)
-    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
+    screen.fill(screen_fill_color)
+    screen.blit(fighter_image, (fighter_x, fighter_y))
+
     pygame.display.update()
-
-
-
-
-
-
