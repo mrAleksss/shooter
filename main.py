07cc427a26139b0721dc1fc69c53620dc46f1f9e
mr_X <mr.Aleksandrsmith@gmail.com>
2023-10-04 15:40:39 +0300

    rocket and aliens

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 import sys
+from random import randint
 
 
 pygame.init()
+
+game_font = pygame.font.Font(None, 30)
 
 screen_width, screen_height = 800, 600
 screen_fill_color = (32, 52, 71)
@@ -10,23 +13,76 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption('Aliens VS Fighter')
 
+FIGHTER_STEP = 0.2
+
 fighter_image = pygame.image.load('images/fighter.png')
 fighter_width, fighter_height = fighter_image.get_size()
 fighter_x, fighter_y = ((screen_width / 2) - (fighter_width / 2)), screen_height - fighter_height
-FIGHTER_STEP = 10
+fighter_is_moving_left, fighter_is_moving_right = False, False
 
-while True:
+ROCKET_STEP = 0.1
+rocket_image = pygame.image.load('images/rocket.png')
+rocket_width, rocket_height = rocket_image.get_size()
+rocket_x, rocket_y = fighter_x + fighter_width/ 2 - rocket_width / 2, fighter_y - rocket_height
+rocket_was_fired = False
+
+ALIEN_STEP = 0.1
+alien_image = pygame.image.load('images/alien.png')
+alien_width, alien_height = alien_image.get_size()
+alien_x, alien_y = randint(0, screen_width - alien_width), 0
+
+game_is_running= True
+
+while game_is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and fighter_x >= FIGHTER_STEP:
-                fighter_x -= FIGHTER_STEP
-            if event.key == pygame.K_RIGHT and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
-                fighter_x += FIGHTER_STEP
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = True
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = True
+            if event.key == pygame.K_SPACE:
+                rocket_was_fired = True
+                rocket_x = fighter_x + fighter_width / 2 - rocket_width / 2
+                rocket_y = fighter_y - rocket_height
 
-                pass
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = False
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = False
+
+    if fighter_is_moving_left and fighter_x >= FIGHTER_STEP:
+        fighter_x -= FIGHTER_STEP
+    if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
+        fighter_x += FIGHTER_STEP
+
+    alien_y += ALIEN_STEP
+
+    if rocket_was_fired and rocket_y + rocket_height < 0:
+        rocket_was_fired = False
+
+    if rocket_was_fired:
+        rocket_y -= ROCKET_STEP
+
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
+    screen.blit(alien_image, (alien_x, alien_y))
+
+    if rocket_was_fired:
+        screen.blit(rocket_image, (rocket_x, rocket_y))
 
     pygame.display.update()
+
+    if alien_y + alien_height > fighter_y:
+        game_is_running = False
+
+game_over_text = game_font.render("Game Over", True, 'yellow')
+game_over_rectangle = game_over_text.get_rect()
+game_over_rectangle.center = (screen_width / 2, screen_height / 2)
+screen.blit(game_over_text, game_over_rectangle)
+pygame.display.update()
+pygame.time.wait(5000)
+
+pygame.quit()
